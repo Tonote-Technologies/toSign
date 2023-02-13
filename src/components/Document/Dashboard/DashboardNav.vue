a806eff06dc8faaf0910b490971c88eb84798be5
Demola <64336052+ekogeda@users.noreply.github.com>
2023-02-13 12:27:42 +0100

    Update on .env.example (Added VUE_APP_URL_SIGN_LINK_LIVE) and DashboardNav (Disable sign-link link)

--- src/components/Document/Dashboard/DashboardNav.vue
+++ src/components/Document/Dashboard/DashboardNav.vue
@@ -123,7 +123,18 @@
             </li>
 
             <li>
-              <a class="d-flex align-items-center" :href="redirectToSignLink + '?status=sign&qt=' + token">
+            <a class="d-flex align-items-center" href="#!">
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link">
+                <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+              </svg>
+              <span class="menu-item text-truncate">Sign link</span>
+              <p title="Coming Soon" class="coming-soon badge rounded-pill badge-light-warning">
+                Coming Soon
+              </p>
+            </a>
+              <!-- <a class="d-flex align-items-center" :href="redirectToSignLink + '?status=sign&qt=' + token">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none"
                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                   class="feather feather-link">
@@ -134,7 +145,7 @@
                 <p title="Coming Soon" class="coming-soon badge rounded-pill badge-light-warning">
                   Coming Soon
                 </p>
-              </a>
+              </a> -->
             </li>
           </ul>
         </li>
